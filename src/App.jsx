4cc15f0e1003037ce029ea3b97607fb0d9c64ccd
RyomenDev <aakashvatsh007@gmail.com>
2025-04-15 18:16:30 +0530

    feat:redux setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
+import { RouterProvider } from "react-router-dom";
+import { useEffect } from "react";
+
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "./store/store.js";
+import { Provider, useDispatch } from "react-redux";
+import store from "./store/store.js";
+// import { jwtDecode } from "jwt-decode";
+// import { login } from "./store/authSlice";
+
 function App() {
-  return <></>;
+  //   const dispatch = useDispatch();
+
+  //   useEffect(() => {
+  // const token =
+  //   localStorage.getItem("token") || sessionStorage.getItem("token");
+  // if (token) {
+  //   try {
+  //     const decodedToken = jwtDecode(token);
+  //     dispatch(login({ email: decodedToken.email, name: decodedToken.name }));
+  //   } catch (error) {
+  //     console.error("Invalid token or token expired", error);
+  //   }
+  // }
+  //   }, [dispatch]);
+
+  return (
+    <>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+         
+        </PersistGate>
+      </Provider>
+    </>
+  );
 }
 
 export default App;
